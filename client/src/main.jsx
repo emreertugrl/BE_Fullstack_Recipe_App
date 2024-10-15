7c94a5acc8d6b0cbb30cd9e345df81106d481996
emreertugrl <emrertugrl@gmail.com>
2024-10-15 12:54:19 +0300

    completed design of recipe creation page, data is sent to the database via POST and displayed on screen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>
 );
